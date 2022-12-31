dde5b1db68df0b7690064425bd929266c5194345
Ari Gibson <hi@arimgibson.com>
2022-12-31 11:41:05 -0700

    Get and log map viewport bounds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-import {locations} from './locations'
-import { useCallback, useRef, useState } from 'react';
+import { locations } from "./locations";
+import { useCallback, useRef, useState } from "react";
 
 const containerStyle = {
   width: "100%",
@@ -15,47 +15,34 @@ const center = {
   lng: -38.523,
 };
 
-
 function App() {
-  const {isLoaded} = useJsApiLoader({
+  const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
-  })
+  });
 
-  const [map, setMap] = useState(null)
-  console.warn("🚀 ~ file: App.js:25 ~ App ~ map", map)
+  const [map, setMap] = useState(null);
+  console.warn("🚀 ~ file: App.js:25 ~ App ~ map", map);
   const onLoad = useCallback((map) => setMap(map), []);
 
-
   const handleMapPosition = () => {
-    // const newass = new window.google.maps.LatLngBounds().getNorthEast().lat()
-    // console.warn("🚀 ~ file: App.js:31 ~ handleMapPosition ~ newass", newass)
-    // console.log(mapRef.current)
-    const ass = new window.google.maps.LatLngBounds();
-    // const bigAss = window.google.maps.ControlPosition.BOTTOM_LEFT;
-    // console.warn("🚀 ~ file: App.js:28 ~ handleMapPosition ~ bigAss", bigAss)
-    // console.warn("🚀 ~ file: App.js:28 ~ handleMapPosition ~ ass", ass)
-    const ne = new window.google.maps.LatLngBounds().getNorthEast();
-    // console.warn("🚀 ~ file: App.js:28 ~ handleMapPosition ~ ne", ne)
-    const sw = new window.google.maps.LatLngBounds().getSouthWest();
-    console.log(3.745 >= sw.lat() && 3.745<= ne.lat());
-    // console.warn("🚀 ~ file: App.js:30 ~ handleMapPosition ~ sw", sw)
-    // console.warn("🚀 ~ file: App.js:28 ~ handleMapPosition ~ bounds", bounds)
-    // const map = new window.google.maps.
-    // console.warn("🚀 ~ file: App.js:28 ~ handleMapPosition ~ map", map)
-  }
-  if (!isLoaded) return 
+    if (!map) return;
+    const ne = map.getBounds().getNorthEast(),
+      sw = map.getBounds().getSouthWest();
+
+    console.log(ne, sw);
+  };
+  if (!isLoaded) return;
   return (
     <div className="App">
-    <GoogleMap 
-      center={center}
-      onLoad={onLoad}
-      // onBoundsChanged={handleMapPosition}
-      onDragEnd={handleMapPosition}
-      mapContainerStyle={containerStyle} zoom={5}>
-    </GoogleMap>
+      <GoogleMap
+        center={center}
+        onLoad={onLoad}
+        onBoundsChanged={handleMapPosition}
+        mapContainerStyle={containerStyle}
+        zoom={5}
+      ></GoogleMap>
     </div>
   );
 }
-
 
 export default App;

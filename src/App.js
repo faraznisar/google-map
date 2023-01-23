67c9d4f71446211ef04be23f0a0fd2f68b6b8afc
faraz <fshah@mindstask.com>
2023-01-23 20:06:06 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,111 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { Autocomplete, GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 
 import { locations } from "./locations";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useState } from "react";
+
 
 const containerStyle = {
   width: "100%",
   height: "500px",
 };
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
-
 function App() {
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+    libraries: ['places']
   });
 
+  const [searchResult, setSearchResult] = useState("Result: none");
+  const [list, setList]  = useState(null);
+  const [center, setCenter] = useState({
+  lat: 65.250856,
+  lng: -76.776675,
+});
   const [map, setMap] = useState(null);
-  console.warn("🚀 ~ file: App.js:25 ~ App ~ map", map);
-  const onLoad = useCallback((map) => setMap(map), []);
+  const onLoad = useCallback((map) => {
+    setMap(map)
+  }, []);
 
+  const onPlaceChanged = () => {
+      if (searchResult != null) {
+        const place = searchResult.getPlace();
+        setCenter({
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
+        });
+      } else {
+        alert("Please enter text");
+      }
+  }
+
+  const onPlaceLoad= (autocomplete) => {
+    setSearchResult(autocomplete);
+  }
   const handleMapPosition = () => {
     if (!map) return;
     const ne = map.getBounds().getNorthEast(),
       sw = map.getBounds().getSouthWest();
 
-    console.log(ne, sw);
+    setList(
+      locations.filter(
+        (item) =>
+          item.latitude >= sw?.lat() &&
+          item.latitude <= ne?.lat() &&
+          item.longitude >= sw?.lng() &&
+          item.longitude <= ne?.lng()
+      )
+    );
+      console.log(ne.lat())
+      console.log(ne.lng())
+      console.log(sw.lat())
+      console.log(sw.lng())
+      
+    // console.log(ne, sw);
   };
+  console.log({list})
   if (!isLoaded) return;
   return (
     <div className="App">
+      <Autocomplete onPlaceChanged={onPlaceChanged} onLoad={onPlaceLoad}>
+        <input
+          type="text"
+          placeholder="Search for Tide Information"
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            width: `240px`,
+            height: `32px`,
+            padding: `0 12px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `14px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+          }}
+        />
+      </Autocomplete>
       <GoogleMap
         center={center}
         onLoad={onLoad}
         onBoundsChanged={handleMapPosition}
         mapContainerStyle={containerStyle}
         zoom={5}
-      ></GoogleMap>
+      >
+        {locations?.map((item, index) => {
+          return (
+            <MarkerF
+              key={index}
+              position={{
+                lat: parseInt(item.latitude),
+                lng: parseInt(item.longitude),
+              }}
+            />
+          );
+        })}
+      </GoogleMap>
     </div>
   );
 }
 
 export default App;
+  
